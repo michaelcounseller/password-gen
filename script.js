84b8a8d276c3a4202f3d8686f6b99531f0dba84c
michaelcounseller <michaelcounseller@gmail.com>
2022-09-15 04:07:44 -0500

    learned about fisher yates algorithm to properly randomize the arrays for a true randomized password. Learned that every object, function and array has a prototype that can be accessed from another object, or array etc. You can create methods that can be accessed from anywhere because the same types have interconnected prototypes and _proto_ properties. Also added a loop to where if user only wants letters then then the for loop that is filling the password array will reset and keep filling till the length is equal to the size of the users character size desire

--- script.js
+++ script.js
@@ -3,50 +3,181 @@
 // Get references to the #generate element
 let generateBtn = document.querySelector("#generate");
 
-let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let lowCaseArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-let capsCaseArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-let specCharaArray = ['!', '"', "'", '@', '#', '$', '%', '^', '&', '*', '(', ')', '[', ']', '{', '}', ';', ':', '/', '\\', '|', '-', '=', '+', '_', '`', '~', ',', '.', '?', '<', '>']
+let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+let lowCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+let capsCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+let specCharaArray = [
+  "!",
+  '"',
+  "'",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "[",
+  "]",
+  "{",
+  "}",
+  ";",
+  ":",
+  "/",
+  "\\",
+  "|",
+  "-",
+  "=",
+  "+",
+  "_",
+  "`",
+  "~",
+  ",",
+  ".",
+  "?",
+  "<",
+  ">",
+];
+
+Array.prototype.shuffle = function () {
+  let i = this.length,
+    randomValue,
+    temp;
+
+  while (--i > 0) {
+    randomValue = Math.floor(Math.random() * (i + 1));
+    temp = this[randomValue];
+    this[randomValue] = this[i];
+    this[i] = temp;
+  }
+  return this;
+};
 
 function generatePassword() {
+  let answerArray = [], outputArray = [], tempArray = [], finalArray = [];
+
   let passLength = parseInt(
     prompt(
       "How many characters would you like your password to be? It is recommended that your character length is MORE THAN 9 and LESS THAN 128!"
     )
   );
 
-    if (passLength <= 0) {
-      alert("Needs to be more than 0!");
+  if (passLength <= 0) {
+    alert("Needs to be more than 0!");
+    generatePassword();
+  } else if (passLength > 128) {
+    alert("Needs to be lower than 128");
+    generatePassword();
+  } else {
+    let capsPrompt = confirm(
+      "Would you like captial letters in your password?"
+    );
+
+    let lowercasePrompt = confirm(
+      "Would you like a lowercase letters in your password?"
+    );
+
+    let specialPrompt = confirm(
+      "Would you like special characters in your password?"
+    );
+
+    let numPrompt = confirm("Would you like numbers in your password?");
+
+    if (!capsPrompt && !lowercasePrompt && !specialPrompt && !numPrompt) {
+      alert("You need to choose one");
       generatePassword();
-    } else if(passLength > 128) {
-      alert('Needs to be lower than 128')
-      generatePassword();
-    } else {
-      prompts();
     }
 
-    console.log(passLength)
+    if(capsPrompt && lowercasePrompt && specialPrompt && !numPrompt) {
+
+    }
+
+    
+
+    // lowCaseArray.shuffle();
+
+    console.log(lowCaseArray);
+
+
+    for (i = 0; i < answerArray.length; i++) {
+      outputArray.push(answerArray[i]);
+      console.log(outputArray);
+      finalArray = tempArray.concat(outputArray)
+
+      if (outputArray.length < passLength) {
+        answerArray.shuffle()
+        i = 0;
+        continue;
+      }
+
+      if (outputArray.length === passLength) {
+        break;
+      }
+    }
+
+    console.log(finalArray);
+
+    //I want to make the array size as long as the passLength value
+  }
 }
 
-
-
-let prompts = function () {
-  let capsPrompt = confirm("Would you like captial letters in your password?");
-
-  let lowercasePrompt = confirm(
-    "Would you like a lowercase letters in your password?"
-  );
-
-  let specialPrompt = confirm(
-    "Would you like special characters in your password?"
-  );
-
-  let numPrompt = confirm("Would you like numbers in your password?");
-
-  if (!capsPrompt && !lowercasePrompt && !specialPrompt && !numPrompt) {
-    prompts();
-  }
-};
+let prompts = function () {};
 
 // Write password to the #password input
 function writePassword() {
